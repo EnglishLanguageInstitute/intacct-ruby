1f87aaaf90f747e8ba74133d5403e53210788266
Erich L Timkar <erich@teamdriveaway.com>
2017-10-23 11:16:09 -0500

    Modifies function_name_from in spec/functions/function_spec_helper.rb to support a function structure like this function/create/glaccount

--- spec/functions/function_spec_helper.rb
+++ spec/functions/function_spec_helper.rb
@@ -11,5 +11,13 @@ def control_id_from(function_xml)
 end
 
 def function_name_from(function_xml)
-  function_xml.xpath('/function').children.first.name
+  
+  child=function_xml.xpath('/function').children.first
+  name = child.name
+  
+  if %w{create update}.include?(name)
+    grand_child_name = child.children.first.name
+    name = "#{name}/#{grand_child_name}"
+  end
+  return name
 end
